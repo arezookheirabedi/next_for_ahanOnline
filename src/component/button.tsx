8690afda0483510358c829bbbc76351fbcccc98f
arezookheirabadi <you@example.com>
2020-12-30 22:10:33 +0330

    fixed git ignore

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import style from '../styles/componentModule/button.module.scss';
- interface IProps{}
- interface IState{}
- export class Button extends React.Component<IProps, IState>{
-     render(){
-         return(
-             <>
-           <div className={ style.main }>
-                       <ul>
-                           <li>
-                               
-                                   <div className={style.circle}>
-                                       {/* <div className={style.blue}> */}
-                                   <i className={`fa fa-twitter ${style.icon}`} ></i>
-                                   {/* </div> */}
-                                   </div>
-                               
-                              </li>
-                        
-                        
-                       </ul>
-                    </div>
-             
-             </>
-         )
-     }
- }
+interface IProps { }
+interface IState { }
+export class Button extends React.Component<IProps, IState>{
+    render() {
+        return (
+            <>
+                <div className={style.main}>
+                    <ul>
+                        <li>
+                            <div className={style.circle}>
+                                <i className={`fa fa-twitter ${ style.icon }`} ></i>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+            </>
+        )
+    }
+}
